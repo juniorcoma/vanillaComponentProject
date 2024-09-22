f9044a120e1bb3d4d51470f712ea7243aaa64843
parkhyeonwoo <pq120wal39@gmail.com>
2024-09-22 23:01:59 +0900

    imgslider 컴포넌트 구현

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,49 @@
+import Component from '../core/Component.js';
+
+export default class ImgSlider extends Component {
+  constructor($target, props) {
+    super($target, props);
+    this.sliderEvent();
+  }
+
+  template() {
+    return `
+      <div class='img-slider_view'>
+        <ul class='img-sliders'>
+          ${[...this.props.imgList, ...this.props.imgList]
+            .map(img => {
+              return `
+                <li class='img-wrap'>
+                  <img class='slider-img' src='${img}' alt='slider img' />
+                </li>
+              `;
+            })
+            .join('')}
+        </ul>
+      </div>
+    `;
+  }
+
+  sliderEvent() {
+    let index = 1;
+    const sliderWidth = this.$target.querySelector('.img-wrap').clientWidth;
+    const sliders = this.$target.querySelector('.img-sliders');
+    const imgListLength = this.props.imgList.length;
+
+    const slide = () => {
+      sliders.style.transition = 'transform 0.5s ease-in-out';
+      sliders.style.transform = `translateX(-${sliderWidth * index}px)`;
+      index++;
+
+      if (index > imgListLength) {
+        setTimeout(() => {
+          sliders.style.transition = 'none';
+          sliders.style.transform = `translateX(0px)`;
+          index = 1;
+        }, 500);
+      }
+    };
+
+    setInterval(slide, 5000);
+  }
+}
